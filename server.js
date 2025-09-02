032dea7176a9b90290cb7597f46110bad3fba02d
Dhruv180604 <142178317+Dhruv180604@users.noreply.github.com>
2025-09-02 17:35:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const axios = require('axios');
+const path = require('path');
 
 // Load environment variables from .env
 dotenv.config();
@@ -12,9 +13,11 @@ const PORT =  process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'source')));
+
 
 app.get('/', (req, res) => {
-  res.send('✅ Healthcare Server is running!');
+  res.sendFile(path.join(__dirname, 'source', 'index.html'));
 });
 
 // POST endpoint to handle user chat
